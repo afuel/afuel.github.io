a902bf415788bfb7052b1bead018e9b432c0653c
afuel <76572491+afuel@users.noreply.github.com>
2021-12-15 18:02:51 -0500

    Update grade_calculator.js

--- grade_calculator.js
+++ grade_calculator.js
@@ -1,3 +1,17 @@
+//When calculate button is clicked, exam grade will be calculated and displayed
+document.getElementById('calculate-overlay').addEventListener("click", function(){
+  if (examGrade() < 0){
+    document.getElementById('output').innerHTML = 'You need at least a 0% on your final exam.';
+  }
+  else if (examGrade() <= 100){
+    document.getElementById('output').innerHTML = 'You need at least a ' + examGrade() +  '% on your final exam.';
+  }
+  else if (examGrade() > 100){
+    document.getElementById('output').innerHTML = 'You cannot achieve your desired grade.';
+  }
+
+});
+
 //Calculates minimum exam grade to achieve desired final grade
   function examGrade(){
     var minExamGrade;
